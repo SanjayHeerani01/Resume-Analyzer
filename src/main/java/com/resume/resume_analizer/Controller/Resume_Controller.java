0d50cf553fb9343a14520937694c9e0e9c343571
SanjayHeerani <lohanasanjay460@gmail.com>
2026-06-13 17:19:27 +0500

    Fixing local host port number

--- src/main/java/com/resume/resume_analizer/Controller/Resume_Controller.java
+++ src/main/java/com/resume/resume_analizer/Controller/Resume_Controller.java
@@ -19,7 +19,7 @@ import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/files")
-@CrossOrigin(origins = "http://localhost:8080/")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class Resume_Controller {
 
 
